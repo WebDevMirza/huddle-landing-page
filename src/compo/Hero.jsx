91a476fdf28a3279b7f4beb69f13105f2c44fcfb
WebDev Mirza <webdevmirza@gmail.com>
2023-01-15 15:06:40 +0600

    adding button shadow

--- src/compo/Hero.jsx
+++ src/compo/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
               Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create
               connections with your users as you engage in genuine discussion.{" "}
             </p>
-            <a className="btn" href="#">
+            <a className="btn box-shadow-2" href="#">
               Register
             </a>
           </div>
